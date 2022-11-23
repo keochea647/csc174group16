8e88c003f3ae24c625b747a5e03298bdaaa4e23c
Keochea <70778547+keochea647@users.noreply.github.com>
2022-11-22 23:57:58 -0800

    Update table.php

--- table.php
+++ table.php
@@ -1,16 +1,51 @@
-<?php
-    require("sqlConnect.php");
-    $sql = "SELECT * FROM USER";
-    $result = $conn->query($sql);
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+    <h1>Users</h1>
+    <table>
+        <thead>
+            <tr>
+                <th>UID</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Sex</th>
+                <th>Street</th>
+                <th>City</th>
+                <th>State</th>
+                <th>ZIP</th>
+                <th>Email</th>
+            </tr>
+        </thead>
+        <?php
+            require("sqlConnect.php");
+            $sql = "SELECT * FROM USER";
+            $result = $conn->query($sql);
 
-    if ($result->num_rows > 0) {
-        // output data of each row
-        while($row = $result->fetch_assoc()) {
-            echo "uid: " . $row["uid"]. " - Name: " . $row["fname"]. " " . $row["lname"]. " - Sex " .$row["sex"];
-        }
-    } else {
-        echo "0 results";
-    }
-    $conn->close();
-    include("navigation.php");
-?> 
+            if ($result->num_rows > 0) {
+                // output data of each row
+                while($row = $result->fetch_assoc()) {
+                    echo
+                        "<tr>
+                            <td>{$row['uid']}</td>
+                            <td>{$row['fname']}</td>
+                            <td>{$row['lname']}</td>
+                            <td>{$row['sex']}</td>
+                            <td>{$row['street']}</td>
+                            <td>{$row['city']}</td>
+                            <td>{$row['state']}</td>
+                            <td>{$row['zip']}</td>
+                            <td>{$row['email']}</td>
+                        </tr>\n";
+                }
+            } else {
+                echo "0 results";
+            }
+            $conn->close();
+            include("navigation.php");
+        ?> 
+    </table>
+    </body>
+</html> 
